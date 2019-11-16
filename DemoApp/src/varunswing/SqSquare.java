fdc2427c6e775ec508a47fc9f4175d491441fd2e
varunswing <varuns939@gmail.com>
2019-11-16 21:10:33 +0530

    Update SqSquare.java

--- DemoApp/src/varunswing/SqSquare.java
+++ DemoApp/src/varunswing/SqSquare.java
@@ -3,6 +3,7 @@ package varunswing;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,11 +11,20 @@ import javax.servlet.http.HttpServletResponse;
 public class SqSquare extends HttpServlet{
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
-		int k = (int)req.getAttribute("k");
+		int k = 0;
+		Cookie cookies[] = req.getCookies();
+		
+		for(Cookie c : cookies) {
+			if(c.getName().equals("k"))
+				k = Integer.parseInt(c.getValue());
+		}
+		
 		k = k*k;
 		
 		PrintWriter out = res.getWriter();
-		out.println("Square of sum is: " + k	);
+		out.println("Square of sum is: " + k);
+		
+		System.out.println("So called");
 	}
 
 }
