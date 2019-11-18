556d89405b9727dbe76f19c4a463ee22c390d40d
varunswing <varuns939@gmail.com>
2019-11-18 18:46:53 +0530

    Create Student.java

---
+++ DemoApp/src/varunswing/Student.java
@@ -0,0 +1,45 @@
+package varunswing;
+
+public class Student {
+	int rollno;
+	String name;
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Student [rollno=" + rollno + ", name=" + name + "]";
+	}
+
+
+
+	public int getRollno() {
+		return rollno;
+	}
+
+
+
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public Student(int rollno, String name) {
+		super();
+		this.rollno = rollno;
+		this.name = name;
+	}
+}
